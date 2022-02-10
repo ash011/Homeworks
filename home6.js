08e9f85155a1691924c8fa7e8b79cda06c29b051
ash001 <muradyan-ashot@mail.ru>
2022-02-10 22:43:56 +0400

    Changed home6.js

--- home6.js
+++ home6.js
@@ -8,15 +8,45 @@ if (!direct) {
 }
 const directPath = path.resolve(direct);
 const textFilePath = path.join(directPath, "sorted_files.txt");
+const filesArray = [];
 
-const files = fs.readdirSync(directPath);
-const textArray = files.map((file) => {
-  let filePath = path.join(directPath, file);
-  filePath += `--->${fs.statSync(filePath).size / 1000}kb\n`;
-  return filePath;
+class File {
+  constructor(name, size) {
+    this.path = name;
+    this.size = size;
+  }
+}
+
+function recurs(pathIs) {
+  const files = fs.readdirSync(pathIs);
+  files.forEach((file) => {
+    const filePath = pathIs + "/" + file;
+    const stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      recurs(filePath);
+    } else {
+      const fileSize = stat.size;
+      const fileName = filePath.substring(directPath.length + 1);
+      const fileObj = new File(fileName, fileSize / 1000);
+      filesArray.push(fileObj);
+    }
+  });
+}
+recurs(directPath);
+
+filesArray.sort((a, b) => {
+  if (a.size > b.size) {
+    return -1;
+  }
+  if (a.size < b.size) {
+    return 1;
+  }
+  return 0;
 });
 
-const text = textArray.join("");
+const text = filesArray
+  .map((file) => `${file.path} - ${file.size}kb`)
+  .join("\n");
 
 fs.writeFile(textFilePath, text, (error) => {
   if (error) {
