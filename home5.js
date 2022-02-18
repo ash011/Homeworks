77af90caffd45acf66234ac7f8782a1ab30e1a4d
ash001 <muradyan-ashot@mail.ru>
2022-02-18 16:40:13 +0400

    Changed sync metods and added error Error handling

--- home5.js
+++ home5.js
@@ -25,56 +25,57 @@ for (let i = 0; i < 20; i++) {
 }
 
 for (let i in villains) {
-    let villain = villains[i]; 
-    versus(villain);
+  let villain = villains[i];
+  versus(villain);
 }
 
-function versus(villain, bat = ()=>{}) {
+function versus(villain, bat = () => {}) {
   let hero = heroes[Math.floor(Math.random() * heroes.length)];
   villain.vs = hero.name;
   hero.vs = villain.name;
-  bat()
+  bat();
 }
 
-function vsBatt(){
-  if(heroes.length === 0 || villains.length === 0){
+function vsBatt() {
+  if (heroes.length === 0 || villains.length === 0) {
     let text = "";
-    if(heroes.length === 0){
-      text += "Villains win\n"
-      for(let i in villains){
-        text += villains[i].name + `[` + villains[i].health + `]  `
+    if (heroes.length === 0) {
+      text += "Villains win\n";
+      for (let i in villains) {
+        text += villains[i].name + `[` + villains[i].health + `]  `;
       }
-      console.log(text)
-      return
-    }else if(villains.length === 0){
-      text += "Heroes win\n"
-      for(let i in heroes){
-        text += heroes[i].name + `[` + heroes[i].health + `]  `
+      console.log(text);
+      return;
+    } else if (villains.length === 0) {
+      text += "Heroes win\n";
+      for (let i in heroes) {
+        text += heroes[i].name + `[` + heroes[i].health + `]  `;
       }
-      console.log(text)
-      return
+      console.log(text);
+      return;
     }
   }
-  
-  let vil = villains[0], her = {};
+
+  let vil = villains[0],
+    her = {};
   let num = 0;
-  let thereIs = true
+  let thereIs = true;
   for (let i in heroes) {
     if (vil.vs === heroes[i].name) {
-        thereIs = false
-        her = heroes[i];
-        num = i
-        break
+      thereIs = false;
+      her = heroes[i];
+      num = i;
+      break;
     }
   }
-  
-  if(thereIs){
-    versus(vil, vsBatt)
-  }else{
-    battle(vil, her, num)
+
+  if (thereIs) {
+    versus(vil, vsBatt);
+  } else {
+    battle(vil, her, num);
   }
 }
-vsBatt()
+vsBatt();
 
 function battle(vil, her, num) {
   let time1 = 0;
@@ -82,29 +83,33 @@ function battle(vil, her, num) {
   let timer = setInterval(() => {
     time1++;
     time2++;
-    if (time1 >= 1/vil.speed * 500) {
+    if (time1 >= (1 / vil.speed) * 500) {
       her.health = her.health - vil.power;
-      console.log(`${vil.name}[${vil.health}] hits ${her.name}[${her.health}]  with a power of ${vil.power}`)
+      console.log(
+        `${vil.name}[${vil.health}] hits ${her.name}[${her.health}]  with a power of ${vil.power}`
+      );
       time1 = 0;
       if (her.health <= 0) {
         her.health = 0;
         heroes.splice(num, 1);
-        console.log(her.name + " dies")
+        console.log(her.name + " dies");
         clearInterval(timer);
-        vsBatt()
+        vsBatt();
       }
     }
-    if (time2 >= 1/her.speed * 500 ) {
+    if (time2 >= (1 / her.speed) * 500) {
       vil.health = vil.health - her.power;
-      console.log(`${her.name}[${her.health}] hits ${vil.name}[${vil.health}]  with a power of ${her.power}`)
+      console.log(
+        `${her.name}[${her.health}] hits ${vil.name}[${vil.health}]  with a power of ${her.power}`
+      );
       time2 = 0;
       if (vil.health <= 0) {
         vil.health = 0;
-        villains.shift()
-        console.log(vil.name + " dies")
+        villains.shift();
+        console.log(vil.name + " dies");
         clearInterval(timer);
-        vsBatt()
+        vsBatt();
       }
     }
   }, 1);
-};
+}
--- home6.js
+++ home6.js
@@ -17,39 +17,46 @@ class File {
   }
 }
 
-function recurs(pathIs) {
-  const files = fs.readdirSync(pathIs);
-  files.forEach((file) => {
-    const filePath = pathIs + "/" + file;
-    const stat = fs.statSync(filePath);
-    if (stat.isDirectory()) {
-      recurs(filePath);
-    } else {
-      const fileSize = stat.size;
-      const fileName = filePath.substring(directPath.length + 1);
-      const fileObj = new File(fileName, fileSize / 1000);
-      filesArray.push(fileObj);
-    }
-  });
+async function recurs(pathIs) {
+  try {
+    const files = await fs.promises.readdir(pathIs);
+    files.forEach(async (file) => {
+      const filePath = pathIs + "/" + file;
+      const stat = await fs.promises.stat(filePath);
+      if (stat.isDirectory()) {
+        recurs(filePath);
+      } else {
+        const fileSize = stat.size;
+        const fileName = filePath.substring(directPath.length + 1);
+        const fileObj = new File(fileName, fileSize / 1000);
+        filesArray.push(fileObj);
+        writeFile();
+      }
+    });
+  } catch (error) {
+    console.log("No such directory");
+  }
 }
 recurs(directPath);
 
-filesArray.sort((a, b) => {
-  if (a.size > b.size) {
-    return -1;
-  }
-  if (a.size < b.size) {
-    return 1;
-  }
-  return 0;
-});
+function writeFile() {
+  filesArray.sort((a, b) => {
+    if (a.size > b.size) {
+      return -1;
+    }
+    if (a.size < b.size) {
+      return 1;
+    }
+    return 0;
+  });
 
-const text = filesArray
-  .map((file) => `${file.path} - ${file.size}kb`)
-  .join("\n");
+  const text = filesArray
+    .map((file) => `${file.path} - ${file.size}kb`)
+    .join("\n");
 
-fs.writeFile(textFilePath, text, (error) => {
-  if (error) {
-    throw error;
-  }
-});
+  fs.writeFile(textFilePath, text, (error) => {
+    if (error) {
+      throw error;
+    }
+  });
+}
